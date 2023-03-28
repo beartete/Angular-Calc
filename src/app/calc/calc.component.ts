e998093dc185182e8ec46613faf1c43ee7e0f491
beartete <isabellacristinavieira2003@gmail.com>
2023-03-28 20:27:55 -0300

    calc angular

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -7,11 +7,33 @@ import { Component } from '@angular/core';
 })
 export class CalcComponent {
   //Atributos da classe/componente
-  num1 : number = 0;
-  num2 : number = 0;
-  soma : number = 0;
+  num1: number = 0;
+  num2: number = 0;
+  resultado: number = 0;
 
-  somar(){
-    this.soma = this.num1 + this.num2;
+  somar() {
+    this.resultado = this.num1 + this.num2;
+  }
+
+  subtrair() {
+    this.resultado = this.num1 - this.num2;
+  }
+
+  multiplicar() {
+    this.resultado = this.num1 * this.num2;
+  }
+
+  dividir() {
+    if (this.num2 !== 0) {
+      this.resultado = this.num1 / this.num2;
+    } else {
+      alert('Não é possível dividir por zero!');
+    }
+  }
+
+  zerar() {
+    this.num1 = 0;
+    this.num2 = 0;
+    this.resultado = 0;
   }
 }
